f5cdf5b9eae13fbf118e507fe8f5265b0f4ed599
Maksym Duhinov <maximakyla@gmail.com>
2024-11-03 20:35:40 +0200

    Add working hours to method main

--- src/com/gmail/maximakyla/homework_3_1/OnlineShop.java
+++ src/com/gmail/maximakyla/homework_3_1/OnlineShop.java
@@ -3,6 +3,6 @@ package com.gmail.maximakyla.homework_3_1;
 public class OnlineShop {
     public static void main(String[] args) {
         System.out.println("This is online shop");
-
+        System.out.println("Working hours 24/7");
     }
 }
