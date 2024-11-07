fbd3ae0109683ca57ad9059c62e5bc1b17fd8893
Maksym Duhinov <maximakyla@gmail.com>
2024-11-08 00:04:04 +0200

    HomeWork-4.1- Add product No.1 data

--- src/com/gmail/maximakyla/homework_4_1/PriceCalculation.java
+++ src/com/gmail/maximakyla/homework_4_1/PriceCalculation.java
@@ -1,6 +1,22 @@
 package com.gmail.maximakyla.homework_4_1;
 
 public class PriceCalculation {
-    public static void main (String [] args){
+    public static void main(String[] args) {
+
+//      Product No.1
+        String product = "smartphone";
+        String currency = "EUR";
+        byte productNum = 1;
+        double price = 999.99;
+        int totalPcs = 66;
+        int salePeriod = 5;
+        double totalSale = totalPcs * price;
+        double averageSale = totalSale / salePeriod;
+
+        System.out.println("Product No." + productNum + ": " + product);
+        System.out.println("Total sales for " + salePeriod + " days is " + currency + " " + totalSale);
+        System.out.printf("Sales by day is " + currency + " %.2f", averageSale);
+
+
     }
 }
