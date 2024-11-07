5a1b511dba99f8548bca458cc328e166876acfc4
Maksym Duhinov <maximakyla@gmail.com>
2024-11-08 00:05:13 +0200

    HomeWork-4.1 2nd Amendment corrected printf

--- src/com/gmail/maximakyla/homework_4_1/PriceCalculation.java
+++ src/com/gmail/maximakyla/homework_4_1/PriceCalculation.java
@@ -6,17 +6,29 @@ public class PriceCalculation {
 //      Product No.1
         String product = "smartphone";
         String currency = "EUR";
-        byte productNum = 1;
+        byte productNumber = 1;
         double price = 999.99;
-        int totalPcs = 66;
+        int unitSold = 66;
         int salePeriod = 5;
-        double totalSale = totalPcs * price;
+        double totalSale = unitSold * price;
         double averageSale = totalSale / salePeriod;
 
-        System.out.println("Product No." + productNum + ": " + product);
+        System.out.println("Product No." + productNumber + ": " + product);
         System.out.println("Total sales for " + salePeriod + " days is " + currency + " " + totalSale);
-        System.out.printf("Sales by day is " + currency + " %.2f", averageSale);
+        System.out.printf("Sales by day %s is %.2f", currency, averageSale);
 
+//      Product No.2
+        product = "laptop";
+        currency = "USD";
+        productNumber = 2;
+        price = 1458.32;
+        unitSold = 12;
+        salePeriod = 7;
+        totalSale = unitSold * price;
+        averageSale = totalSale / salePeriod;
 
+        System.out.println("\n\nProduct No." + productNumber + ": " + product);
+        System.out.println("Total sales for " + salePeriod + " days is " + currency + " " + totalSale);
+        System.out.printf("Sales by day is %s %.2f" , currency, averageSale);
     }
 }
