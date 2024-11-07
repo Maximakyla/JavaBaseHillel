fab0b34db1c8b8481eaf153c22427176848e110d
Maksym Duhinov <maximakyla@gmail.com>
2024-11-08 00:05:13 +0200

    HomeWork-4.1 Add product No.2 data ==FINAL==

--- src/com/gmail/maximakyla/homework_4_1/PriceCalculation.java
+++ src/com/gmail/maximakyla/homework_4_1/PriceCalculation.java
@@ -17,6 +17,18 @@ public class PriceCalculation {
         System.out.println("Total sales for " + salePeriod + " days is " + currency + " " + totalSale);
         System.out.printf("Sales by day is " + currency + " %.2f", averageSale);
 
+//      Product No.2
+        product = "laptop";
+        currency = "USD";
+        productNum = 2;
+        price = 1458.32;
+        totalPcs = 12;
+        salePeriod = 7;
+        totalSale = totalPcs * price;
+        averageSale = totalSale / salePeriod;
 
+        System.out.println("\n\nProduct No." + productNum + ": " + product);
+        System.out.println("Total sales for " + salePeriod + " days is " + currency + " " + totalSale);
+        System.out.printf("Sales by day is " + currency + " %.2f", averageSale);
     }
 }
