0f0840e28a33474487c15eb246721c9d591a5e0c
Maksym Duhinov <maximakyla@gmail.com>
2024-11-11 15:09:54 +0200

    HomeWork-5.1 FINAL Version

--- src/com/gmail/maximakyla/homework_5_1/IncomeTax.java
+++ src/com/gmail/maximakyla/homework_5_1/IncomeTax.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class IncomeTax {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your total income value for tax calculation");
+        System.out.println("Please enter your total income value for tax calculation in EUR");
         int income = scanner.nextInt();
+        String currency = "EUR";
 
-        if (income <= 10000) {
-            System.out.println("Income tax = " + (income * 0.025));
+        if (income <= 10000){
+            System.out.printf("Income tax %.2f %s" , income * 0.025 , currency);
         } else if (income <= 25000){
-            System.out.println("Income tax = " + (income * 0.043));
+            System.out.printf("Income tax %.2f %s", income * 0.043 , currency);
         } else {
-            System.out.println("Income tax = " + (income * 0.067));
+            System.out.printf("Income tax %.2f %s", income * 0.067 , currency);
         }
     }
 }
